f35c7986ac4f6ebf32496d99fd14bbede48aed9a
leandro <leandro.vicente98@outlook.com>
2020-02-26 16:30:27 -0300

    Configurando Store REDUX

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Provider } from "react-redux";
+import store from "./store";
+const App = () => (
+    <Provider store={store}>
+        <h1>Hellow Word</h1>
+    </Provider>
+);
+
+export default App;
